6e51d352d6ba05bc3a4c427ff3f76a259215bfbd
rogaciano <rogaciano@gmail.com>
2025-12-28 14:29:37 -0300

    chore: delete an unspecified file.

--- backend/crm/models.py
+++ backend/crm/models.py
@@ -332,14 +332,7 @@ class Oportunidade(models.Model):
         blank=True
     )
     
-    indicador_comissao = models.ForeignKey(
-        Contato,
-        on_delete=models.SET_NULL,
-        null=True,
-        blank=True,
-        related_name='indicacoes',
-        help_text='Contato que indicou esta oportunidade'
-    )
+    indicador_comissao = models.CharField(max_length=255, null=True, blank=True)
     
     REGIAO_SUPORTE_CHOICES = [
         ('MATRIZ', 'Matriz'),
